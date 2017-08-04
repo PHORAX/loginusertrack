5706d70fd504db50ac266dd71ed0780b965bc6c0
Felix Kopp <felix-source@phorax.com>
2017-08-04 21:04:47 +0200

    [TASK] Untrack und remove superflous data+comments

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,37 +1,16 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "loginusertrack".
-#
-# Auto generated 13-08-2015 16:40
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
-
 $EM_CONF[$_EXTKEY] = array(
     'title' => 'Login User Tracking',
     'description' => 'Logs in a separate table each time a frontend user logs in and further the timespan of the session and viewed pages. Backend module provides statistics over the data.',
     'category' => 'module',
-    'shy' => 1,
     'version' => '2.1.0',
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'loadOrder' => '',
     'TYPO3_version' => '6.2.0-6.2.99',
-    'PHP_version' => '',
-    'module' => '',
     'state' => 'stable',
     'uploadfolder' => 1,
     'createDirs' => '',
     'modify_tables' => '',
     'clearcacheonload' => 1,
-    'lockType' => '',
     'author' => 'Daniel Minder',
     'author_email' => 'typo3@minder.de',
-    'author_company' => '',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
 );
